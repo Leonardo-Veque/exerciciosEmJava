5ed0058251bcd7ded7e505bf96526cf325cc427e
Leonardo Veque <leonardo5veque@gmail.com>
2023-11-05 19:02:19 -0300

    Mais um exercicio de java

--- src/exercicioVetor.java
+++ src/exercicioVetor.java
@@ -2,6 +2,19 @@ import javax.swing.JOptionPane;
 
 public class exercicioVetor {
     public static void main(String[] args) {
+        int resposta = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero do exercicio"));
+
+        if (resposta == 1) {
+            exercicio1(args);
+
+        } else if (resposta == 2) {
+            exercicio2(args);
+        } else {
+            System.out.println("Não existe esse exercicio");
+        }
+    }
+
+    public static void exercicio1(String[] args) {
         String tamanho = JOptionPane.showInputDialog("Digite o tamanho do vetor");
         int tmanhoVetor = Integer.parseInt(tamanho);
         String[] fala = new String[tmanhoVetor];
@@ -12,6 +25,20 @@ public class exercicioVetor {
 
         for (int i = 0; i < fala.length; i++) {
             System.out.println(fala[i]);
+        }
+    }
+
+    public static void exercicio2(String[] args) {
+        int tamanho = Integer.parseInt(JOptionPane.showInputDialog("Digite o tamanho do vetor"));
+        int[] vetor = new int[tamanho];
+        int impar = 0;
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite os numeros do vetor"));
+            if (vetor[i] % 2 != 0) {
+                impar = vetor[i];
+                System.out.println("Numeros impares:" + impar);
+            }
         }
 
     }
